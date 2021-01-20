8aba66b468221a472538534d9173b1109fdd020d
JuneJH <15310487388@163.com>
2021-01-20 17:27:36 +0800

    动画切换

--- banner/src/components/ImgContainer.js
+++ banner/src/components/ImgContainer.js
@@ -1,11 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types"
 
 export default class ImgContainer extends Component {
+    static propsTypes = {
+        imgSrcs: PropTypes.arrayOf(PropTypes.string).isRequired,
+        imgWidth: PropTypes.number.isRequired,
+        imgHeight: PropTypes.number.isRequired,
+        duration: PropTypes.number.isRequired,
+    }
+    imgContainerRef = React.createRef();
+    tick = 16;
+    timer = null;
+
+    changeContinerPosition(index) {
+        console.log(this.props.imgSrcs.length)
+        if (index < 0) {
+            index = this.props.imgSrcs.length - 1
+        } else if (index > this.props.imgSrcs.length - 1) {
+            index = 0;
+        }
+
+        const targetLeft = - index * this.props.imgWidth;
+
+        let currentLeft = parseFloat(window.getComputedStyle(this.imgContainerRef.current).marginLeft);
+
+        const count = Math.ceil(this.props.duration / this.tick);
+
+        let currentCount = 0;
+
+        const dis = (targetLeft - currentLeft) / count;
+
+        clearInterval(this.timer);
+        this.timer = setInterval(()=>{
+            currentCount ++;
+            currentLeft += dis;
+            this.imgContainerRef.current.style.marginLeft = currentLeft + "px";
+            if(currentCount === count){
+                clearInterval(this.timer);
+                this.imgContainerRef.current.style.marginLeft = targetLeft + "px";
+            }
+            
+        },this.tick)
+    }
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
     render() {
+        const imgs = this.props.imgSrcs.map((src, i) => <img
+            src={src}
+            key={i}
+            alt={i}
+            style={{
+                width: this.props.imgWidth,
+                height: this.props.imgHeight,
+                float: 'left',
+            }} />)
         return (
-            <div>
-                
-            </div>
+                <div
+                    className="img-container"
+                    ref={this.imgContainerRef}
+                    style={{
+                        width: this.props.imgWidth * this.props.imgSrcs.length,
+                        height: this.props.imgHeight
+                    }}
+                >
+                    {imgs}
+
+
+                </div>
         )
     }
 }
--- banner/src/index.js
+++ banner/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import img1 from "./assets/img/1.jpg"
+import img2 from "./assets/img/2.webp"
+import img3 from "./assets/img/3.jpg"
+import img4 from "./assets/img/4.jpg"
+import img5 from "./assets/img/5.webp"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App imgSrcs={[img1,img2,img3,img4,img5]} />
   </React.StrictMode>,
   document.getElementById('root')
 );
