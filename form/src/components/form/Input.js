31f689bd087299c0670afe87915324cc8ec6d612
JuneJH <15310487388@163.com>
2021-02-03 23:36:06 +0800

    添加inputzu'jian

--- form/src/components/form/Input.js
+++ form/src/components/form/Input.js
@@ -5,7 +5,7 @@ export default class Input extends Component {
     render() {
         return (
            <Consumer>
-               {context=>(<input value={context[this.props.name]} onChange={e=>{
+               {context=>(<input value={context.formData[this.props.name]} name={this.props.name} type={this.props.type} onChange={e=>{
                    context.onChange(this.props.name,e.target.value)
                }}/>)}
            </Consumer>
