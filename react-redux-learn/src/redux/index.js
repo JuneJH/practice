943f34a117a5047a8a7ff6add44d37c5e204992b
JuneJH <15310487388@163.com>
2021-03-15 20:35:27 +0800

    redux

--- react-redux-learn/src/redux/index.js
+++ react-redux-learn/src/redux/index.js
@@ -1,33 +1,45 @@
-import { createStore } from 'redux';
+import { createStore,bindActionCreators } from 'redux';
 import { INCREASE, DECREASE, SET } from './action/type';
-import { getDecreaseAction, getIncreaseAction, getSetAction } from "./action"
+import * as loginAction from './action/loginAction'
+import * as userAction from './action/userAction'
+import reducer from './reducer'
 
 
-function reducer(state, action) {
-    if (action.type === INCREASE) {
-        return state + 1;
-    } else if (action.type === DECREASE) {
-        return state - 1;
-    } else if (action.type === SET) {
-        return action.payload;
-    }
-    return state;
-}
+// function reducer(state, action) {
+//     if (action.type === INCREASE) {
+//         return state + 1;
+//     } else if (action.type === DECREASE) {
+//         return state - 1;
+//     } else if (action.type === SET) {
+//         return action.payload;
+//     }
+//     return state;
+// }
 
 const store = createStore(reducer, 1);
 
-console.log("初始",store.getState());
-store.dispatch(getDecreaseAction());
-console.log("减一后",store.getState());
+console.log(store)
+
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+// 绑定action和store
+const boundAction = bindActionCreators({...loginAction,...userAction},store.dispatch);
+
+boundAction.createLoginAction({loginUser:"june"})
+boundAction.createAddUserAction({id:3,name:"add",age:12})
+boundAction.createDeleteUserAction(1);
+boundAction.createUpdateUserAction(2,{name:"update",age:11})
 
 
-console.log("初始",store.getState());
-store.dispatch(getSetAction(20));
-console.log("设置",store.getState());
+// console.log("初始",store.getState());
+// boundAction.getSetAction(20)
+// console.log("设置",store.getState());
 
-console.log("初始",store.getState());
-store.dispatch(getIncreaseAction());
-console.log("加一后",store.getState());
+// console.log("初始",store.getState());
+// boundAction.getIncreaseAction()
+// console.log("加一后",store.getState());
 
 
 
