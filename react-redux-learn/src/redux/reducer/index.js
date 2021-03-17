b81db0d016dfb5dcf87dd95b7aea47a494dacb7d
JuneJH <15310487388@163.com>
2021-03-17 13:08:26 +0800

    实现reducer

--- react-redux-learn/src/redux/reducer/index.js
+++ react-redux-learn/src/redux/reducer/index.js
@@ -1,12 +1,13 @@
 import loginReducer from './loginReducer';
 import userReducer from './usersReducer';
-import {combineReducers} from 'redux';
+// import {combineReducers} from 'redux';
+import {combineReducers} from '../../myRedux'
 
-export default function (state={},action){
-    return {
-        loginReducer:loginReducer(state.loginReducer,action),
-        userReducer:userReducer(state.userReducer,action)
-    }
-}
+// export default function (state={},action){
+//     return {
+//         loginReducer:loginReducer(state.loginReducer,action),
+//         userReducer:userReducer(state.userReducer,action)
+//     }
+// }
 
-// export default combineReducers({loginReducer,userReducer})
+export default combineReducers({loginReducer,userReducer})
