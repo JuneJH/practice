ee8302b42ce4d3335293a5a82e4aee9272011691
JuneJH <15310487388@163.com>
2021-04-18 20:27:17 +0800

    init

--- dva-demo/src/index.js
+++ dva-demo/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {dva} from 'dva';
+
+const dvaApp = dva();
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
