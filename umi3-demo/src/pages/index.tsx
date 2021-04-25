1e1e131ecbb9ab1917a1824a06a869882c7b5697
JuneJH <15310487388@163.com>
2021-04-25 22:59:43 +0800

    学生列表

--- umi3-demo/src/pages/index.tsx
+++ umi3-demo/src/pages/index.tsx
@@ -1,13 +1,23 @@
-import axios from 'axios';
+import { getStudent } from '@/services/api';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import styles from './index.less';
 
 export default function IndexPage() {
-  axios.get("/api/student").then(res=>{
-    console.log(res);
-  })
+  let dom = null;
+  const [data, setData] = useState<any>([]);
+  dom = data.map((itme: any) => (
+    <div key={itme.id}>{itme.name}</div>
+    ));
+  console.log(dom)
+  useEffect(() => {
+    getStudent().then((res) => {
+      setData(res.data.data);
+    });
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.container}>{dom}</h1>
     </div>
   );
 }
--- umi3-demo/src/services/api.ts
+++ umi3-demo/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
+    baseURL:"/api",
     headers:{
         authorization:window.localStorage.getItem("token")
     }
