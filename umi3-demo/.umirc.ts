7e8f6bcae067226693a0cfc95a7ab223578125b6
JuneJH <15310487388@163.com>
2021-04-25 13:15:53 +0800

    配置请求代理

--- umi3-demo/.umirc.ts
+++ umi3-demo/.umirc.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  proxy:{
+    "/api":{
+      "target":"http://121.36.51.141:8080",
+      "changeOrigin":true,
+      "pathRewrite":{'^/api':""}
+    }
+  },
   fastRefresh: {},
 });
--- umi3-demo/src/pages/index.tsx
+++ umi3-demo/src/pages/index.tsx
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import styles from './index.less';
 
 export default function IndexPage() {
+  axios.get("/api/student").then(res=>{
+    console.log(res);
+  })
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
