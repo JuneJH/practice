841278321f979712ce5ef335e80c86401a6196dc
JuneJH <15310487388@163.com>
2021-07-28 22:59:24 +0800

    学习vue代码示例、插槽

---
+++ vue-app-1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>APP</h1>
+    <p>描述：</p>
+    <Hello>
+      <template>
+        替换插槽
+      </template>
+      <template v-slot:title>
+        替换具名插槽
+      </template>
+      <template #data="data1" > 
+        <h1>替换作用域插槽：{{data1.data}}</h1>
+      </template>
+    </Hello>
+  </div>
+</template>
+<script>
+import Hello from "./components/Helloorld.vue";
+export default {
+  components:{Hello},
+  data(){
+    return {
+      data:"我是父组件数据"
+    }
+  }
+}
+</script>
---
+++ vue-app-1/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <slot>我是插槽</slot>
+    </div>
+    <div>
+      <slot name="title">我是具名插槽</slot>
+    </div>
+    <div>
+      <slot name="data" data="子组件数据">作用域插槽</slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      data:"我是子组件的数据"
+    }
+  }
+}
+</script>
