70431ad299b73a2a12c27ea2be0133f7b4493a73
JuneJH <15310487388@163.com>
2021-07-30 23:44:24 +0800

    添加vue-router

--- vue-app-1/src/App.vue
+++ vue-app-1/src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div>
-    <h1>APP</h1>
-    <p>描述：</p>
-    <Hello>
-      <template>
-        替换插槽
-      </template>
-      <template v-slot:title>
-        替换具名插槽
-      </template>
-      <template #data="data1" > 
-        <h1>替换作用域插槽：{{data1.data}}</h1>
-      </template>
-    </Hello>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-<script>
-import Hello from "./components/HelloWorld.vue";
-export default {
-  components:{Hello},
-  data(){
-    return {
-      data:"我是父组件数据"
-    }
-  }
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
