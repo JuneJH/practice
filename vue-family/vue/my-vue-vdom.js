8008727ecc320c1c62a4508ebc591a410a23da11
JuneJH <15310487388@163.com>
2021-08-13 23:54:43 +0800

    mini-vue实现

--- vue-family/vue/my-vue-vdom.js
+++ vue-family/vue/my-vue-vdom.js
@@ -9,73 +9,122 @@ class JVue {
         this.$data = typeof options.data === "function" ? options.data() : options.data;
         new Observe(this.$data);
         proxy(this);
-        this.mountComponent();
+        if (options.el) {
+            this.mountComponent(options.el);
+        }
     }
-    mountComponent() {
+    mountComponent(el) {
+        this.$el = document.querySelector(el || "body");
         let updateComponent = () => {
-            const parent = document.querySelector(this.$options.el || "body");
-            let oldVnode = parent;
-            if (this.vnode) {
-                oldVnode = { ...this.vnode }
-            }
             this.vnode = this.$options.render.call(this, createVnode);
-            this.vnode.parent = parent;
-            this.patch(oldVnode, this.vnode);
+            this._update(this.vnode)
         }
         new Watcher(this, updateComponent)
     }
-    patch(oldVnode, vnode) {
-        if (oldVnode.nodeType) {
-            this.update(vnode,this.vnode.parent);
+    _update(vnode) {
+        const prevEl = this.$el;
+        const prevVnode = this._vnode;
+        if (prevVnode) {
+            this.$el = this.__patch__(prevVnode, vnode);
         } else {
-            this.patchVnode(oldVnode, vnode);
+            this.$el = this.__patch__(prevEl, vnode);
+        }
+
+    }
+    __patch__(oldVnode, vnode) {
+        const isRealElement = oldVnode.nodeType;
+        if (!isRealElement && oldVnode.tag === vnode.tag) {
+            this.patchVnode(oldVnode, vnode)
+        } else {
+           if(isRealElement){
+             oldVnode = new Vnode(oldVnode.tagName, null, null, oldVnode)
+           }
+            const oldElm = oldVnode.elm;
+            const parentElm = oldElm.parentNode;
+            this.createElm(vnode, parentElm, oldElm);
+            if (parentElm && oldElm) {
+                parentElm.removeChild(oldElm)
+            }
+        }
+        this._vnode = vnode;
+        return vnode.elm
+    }
+    createElm(vnode, parent, refElm) {
+        const data = vnode.data;
+        const tag = vnode.tag;
+        const children = vnode.children;
+        vnode.elm = createElement(tag);
+        if (children) {
+            this.cretaeChildren(vnode, children);
+        }
+        parent.insertBefore(vnode.elm, refElm)
+    }
+    cretaeChildren(vnode, children) {
+        if (Array.isArray(children)) {
+            children.forEach(child => {
+                if (typeof child === "object") {
+                    this.createElm(child, vnode.elm, null)
+                } else {
+                    vnode.elm.appendChild(createTextNode(String(child)))
+                }
+            })
+        } else {
+            vnode.elm.appendChild(createTextNode(String(children)))
         }
     }
     patchVnode(oldVnode, vnode) {
-        if (oldVnode === vnode) return;
-        if (oldVnode.tag === vnode.tag) {
-            this.updateAttr(oldVnode, vnode)
-        }
-    }
-    updateAttr(oldVnode, vnode) {
-        if (oldVnode.elm) {
-            oldVnode.elm.innerText = vnode.data;
-            this.vnode = oldVnode;
-        }
-    }
-    update(vnode, parent = null) {
-        if (vnode instanceof Vnode) {
-            vnode.elm = this._update(vnode,parent);
-            vnode.parent = vnode.parent || parent;
-        }
-    }
-    _update(v,parent) {
-  
-        if (tags.includes(v.tag)) {
-            const dom = document.createElement(v.tag);
-            if (v.children) {
-                v.children.forEach(vnode=>{
-                    this.update(vnode, dom);
-                })
+        const elm = vnode.elm = oldVnode.elm;
+        const oldch = oldVnode.children;
+        const ch = vnode.children;
+        if(oldch === ch){
+            return;
+        }else if(typeof oldch !== "object"){
+            if(typeof ch !== "object"){
+               elm.textContent = ch;
+            }else{
+                elm.innerHTML = "";
+                ch.forEach(c=>this.createElm(c,elm,null))
             }
-            dom.innerText = v.data;
-            parent.appendChild(dom);
-            return dom;
+        } else if(typeof oldch == "object"){
+            if(typeof ch !== "object"){
+                elm.textContent = ch;
+            }else{
+                this.updateChildren(elm,oldch,ch);
+            }
         }
-        
+    }
+    updateChildren(elm,oldch,ch){
+        const oldLen = oldch.length;
+        const vlen = ch.length;
+        const len = Math.min(oldLen,vlen);
+        for(let i = 0; i < len; i ++){
+            const oldV = oldch[i];
+            const v = ch[i];
+           if(typeof oldV === "object" && typeof v === "object"){
+               this.__patch__(oldV,v)
+           }
+        }
+        if(oldLen === vlen){
+            return
+        }else if(oldLen > vlen){
+            oldch.slice(len).forEach(old=>elm.removeChild(old.elm))
+        }else{
+            ch.slice(len).forEach(c=>{
+                this.createElm(c,elm,null)
+            })
+        }
+
     }
 }
-
-
 function createVnode(tag, data, child) {
     return new Vnode(tag, data, child)
 }
 class Vnode {
-    constructor(tag, data, child) {
+    constructor(tag, data, child, elm) {
         this.tag = tag;
         this.data = data;
         this.children = child;
-        this.elm = null;
+        this.elm = elm;
         this.parent = null;
     }
 }
@@ -154,8 +203,6 @@ class Watcher {
     }
 }
 
-
-
 function defineReactive(obj, key, val) {
     const dep = new Dep();
     let ch = observe(val);
@@ -192,4 +239,11 @@ function observe(obj) {
     }
     return ob;
 
+}
+
+function createElement(tag) {
+    return document.createElement(tag)
+}
+function createTextNode(text) {
+    return document.createTextNode(text)
 }
