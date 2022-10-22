ea8c7e724c151a67c897efe524aa00f5aa6f9eb7
JuneJH <15310487388@163.com>
2022-10-22 21:51:45 +0800

    feat(*): 冲

--- react/form/src/components/formV2/form.js
+++ react/form/src/components/formV2/form.js
@@ -2,6 +2,11 @@ import { FormProvider } from "./formContext";
 import React from "react";
 import useForm from "./useForm";
 
+/**
+ * 自定义form
+ * @param {*} param0 
+ * @returns 
+ */
 function Form({children}) {
     const [formInstance] = useForm();
     return <form onSubmit={e=>{
